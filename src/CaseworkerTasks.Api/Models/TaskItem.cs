c26d15d6dc62d47b69758de62852c5e898bf4faa
Jomin John <jominjhn@googlemail.com>
2025-09-29 14:28:23 +0100

    Creating TaskItem model and TaskStatus enum

---
+++ src/CaseworkerTasks.Api/Models/TaskItem.cs
@@ -0,0 +1,10 @@
+namespace CaseworkerTasks.Api.Models;
+
+public class TaskItem
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public TaskStatus Status { get; set; }
+    public DateTime? DueAt { get; set; }
+}
---
+++ src/CaseworkerTasks.Api/Models/TaskStatus.cs
@@ -0,0 +1,8 @@
+namespace CaseworkerTasks.Api.Models;
+
+public enum TaskStatus
+{
+    ToDo = 0,
+    InProgress = 1,
+    Done = 2
+}
